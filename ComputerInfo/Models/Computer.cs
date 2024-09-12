ee5e669a523ac92ff9d115ce1c19325e74d1da10
Stephen <633706+Merganser2@users.noreply.github.com>
2024-09-11 23:29:28 -0700

    Add Id to Computer Model

--- ComputerInfo/Models/Computer.cs
+++ ComputerInfo/Models/Computer.cs
@@ -2,8 +2,11 @@ namespace ComputerInfo.Models;
 
 public class Computer
 {
+    // We didn't have a PK originally, inserting data with Dapper, but SQL (?) created one for us; adding that
+    //  field, ComputerId, to the Model
+    public int ComputerId { get; set; } 
     public string Motherboard { get; set; } = "";
-    public int CPUCores { get; set; }
+    public int? CPUCores { get; set; } = 0;
     public bool HasWifi { get; set; }
     public bool HasLTE { get; set; }
     public DateTime ReleaseDate { get; set; }
