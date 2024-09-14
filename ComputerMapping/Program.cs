80746e14f478e6d56b11ace06324dfab35bb9c39
Stephen <633706+Merganser2@users.noreply.github.com>
2024-09-14 13:34:03 -0700

    ComputerMapping project

---
+++ ComputerMapping/Program.cs
@@ -0,0 +1,19 @@
+﻿// Playing with restoring Program.cs to before .NET 6 
+// See https://aka.ms/new-console-template for more information
+// The namespace, which is project name, is implicit by default
+namespace ComputerMapping;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // DbInteractions dbInteractions = new DbInteractions();
+
+        // dbInteractions.InsertComputerInfo();
+
+        // dbInteractions.GetAllComputersComputerInfo();
+
+        // dbInteractions.TestDbConnectivity();
+    }
+}
+
